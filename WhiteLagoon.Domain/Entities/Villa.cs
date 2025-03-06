1171b48fca4b36a65ca39697be406e8bedc44cae
Kumar Amit <techaltumnoida15@gmail.com>
2025-03-06 23:09:08 +0530

    Add project files.

---
+++ WhiteLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public 
+    }
+}
